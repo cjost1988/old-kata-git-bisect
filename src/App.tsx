0de48a70b5245ca5aa809dfc129c8abea0b2cdd5
Christian Jost <christian.jost@schuettflix.com>
2022-11-01 21:01:28 +0100

    feat: introduce product list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './App.css';
 
+const products: Array<Product> = [
+    {
+        id: 12,
+        title: 'Schotter',
+        price: 13.37,
+    },
+    {
+        id: 13,
+        title: 'Sand',
+        price: 73.31,
+    },
+    {
+        id: 14,
+        title: 'Kies',
+        price: 71.37,
+    },
+    {
+        id: 15,
+        title: 'RCL',
+        price: 99.99,
+    },
+];
+
 function App() {
   return (
     <div className="App">
-
+        <ul>
+            {products.map((product: Product) => <li>
+                {product.title}
+            </li>)}
+        </ul>
     </div>
   );
 }
